c05cacb249af3f35c5be751acd1049805dad7519
19sharda <bestprakash.1@gmail.com>
2025-01-31 23:21:31 +0530

    Start Appium Day7

--- src/main/java/org/example/AppiumSetupTest.java
+++ src/main/java/org/example/AppiumSetupTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -16,21 +18,17 @@ public class AppiumSetupTest {
         capabilities.setCapability("uiautomator2ServerInstallTimeout", 60000);
         capabilities.setCapability("adbExecTimeout", 120000);  // Timeout in milliseconds (e.g., 2 minutes)
 
-
-        // Platform Name (Android in this case)
-        capabilities.setCapability("platformName", "Android");
-
-        // Device Name (replace with your device/emulator name)
-        capabilities.setCapability("deviceName", "emulator-5554"); // Or use your real device name
-
-        // Path to your APK file (make sure it's correctly specified for your platform)
-        capabilities.setCapability("app", "C:\\Users\\sp199\\IdeaProjects\\30days_Appium\\sunflower\\app\\build\\outputs\\apk\\debug\\app-debug.apk"); // Replace with the actual path
-
         // Specify the Appium server URL (should match the Appium server you started earlier)
         URL appiumServerUrl = new URL("http://127.0.0.1:4723/");
 
         // Create an instance of AndroidDriver to interact with the app
         AndroidDriver driver = new AndroidDriver(appiumServerUrl, capabilities);
+
+        WebElement addPlantBtn = driver.findElement(By.xpath("//android.widget.TextView[@text='Add plant']/parent::android.view.View"));
+        addPlantBtn.click();
+        WebElement plantListTab = driver.findElement(By.xpath("//android.widget.TextView[@text='Plant list']"));
+        plantListTab.click();
+
 
         // Add your test code here (you can perform actions on your app using the 'driver')
         System.out.println("App launched successfully!");
@@ -39,3 +37,6 @@ public class AppiumSetupTest {
         driver.quit();
     }
 }
+
+
+
---
+++ src/main/java/org/example/LinkedInTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+
+public class LinkedInTest { // ✅ Renamed class to avoid conflict
+    public static void main(String[] args) throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("deviceName", "RZCW902G7RE"); // Use `adb devices` to find it
+        caps.setCapability("appPackage", "com.linkedin.android");
+        caps.setCapability("appActivity", "com.linkedin.android.authenticator.LaunchActivityDefault");
+        caps.setCapability("automationName", "UiAutomator2");
+
+        // ✅ No generic type for AndroidDriver
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), caps);
+
+        // ✅ Find login button and click it
+        WebElement loginBtn = driver.findElement(By.id("com.linkedin.android:id/login"));
+        loginBtn.click();
+
+        System.out.println("Login button clicked successfully!");
+
+        driver.quit();
+    }
+}
